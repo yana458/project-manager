dfc217f75146ce1e827ce736aa5b5c3c9749a82d
Yana Chistyakova <yana@DESKTOP-QNT8ERQ.localdomain>
2026-03-04 09:34:49 +0000

    Project setup: initial setup and base routes

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,9 @@
+<!-- resources/views/dashboard.blade.php -->
+@extends('layouts.app')
+
+@section('title', 'Dashboard')
+
+@section('content')
+  <h1 style="margin-top:0;">Dashboard</h1>
+  <p>Gestión de proyectos en curso.</p>
+@endsection
