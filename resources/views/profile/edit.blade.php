b335bcef3c5a5c58a27c7dd5e9fb080d07e98f8d
Yana Chistyakova <yana@DESKTOP-QNT8ERQ.localdomain>
2026-03-12 13:17:59 +0000

    Hide delete account and show admin contact message for non-admins

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -19,11 +19,14 @@
                 </div>
             </div>
 
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.delete-user-form')
+            @unless(auth()->user()->hasRole('admin'))
+                <div class="mt-6 rounded-xl border bg-white p-4">
+                    <h3 class="text-base font-semibold text-gray-900">Account</h3>
+                    <p class="mt-1 text-sm text-gray-600">
+                        If you need to deactivate your account, please contact an administrator.
+                    </p>
                 </div>
-            </div>
+            @endunless
         </div>
     </div>
 </x-app-layout>
